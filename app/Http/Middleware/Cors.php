fadce0bf7ae0ae7f4f9edd8886554e1c974652ba
Juan <idpelago@gmail.com>
2024-12-31 22:25:37 +0700

    fix cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,8 +16,9 @@ class Cors
     public function handle($request, Closure $next)
     {
         $response = $next( $request );
-        $response->header( 'Access-Control-Allow-Origin', '*' );
-        $response->header( 'Access-Control-Allow-Headers', '*' );
+        $response->header('Access-Control-Allow-Origin', '*');
+        $response->header('Access-Control-Allow-Methods', '*');
+        $response->header('Access-Control-Allow-Headers', '*');
 
         return $response;
     }
